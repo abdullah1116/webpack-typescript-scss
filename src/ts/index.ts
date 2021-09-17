549b6516ff49ec9212d8759a20859d75d91f34bc
tmpabdullah <tmp.abdullah1@outlook.com>
2021-09-17 21:03:58 +0500

    lazy load lazy script

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,9 +1,12 @@
 import '../scss/index.scss';
-import { lazyScript } from './lazyScript';
 
 window.addEventListener('load', () => {
   const headingText = `Webpack 5.28 & TypeScript 4.4 & Scss 1.41`;
   document.getElementById('error')!.innerText = '';
   document.getElementById('heading')!.innerText = headingText;
-  lazyScript();
+  import(
+    /* webpackChunkName: 'lazyScript', webpackPrefetch: true */ './lazyScript'
+  ).then((module) => {
+    module.lazyScript();
+  });
 });
